250907a2a116c21526dacbcae1c57df7aee45b6c
Ruk Doe <info@doany.io>
2023-08-10 01:19:35 +0900

    fix few

--- utils/userModel.ts
+++ utils/userModel.ts
@@ -1,7 +1,4 @@
-import { Condition, ObjectId } from "mongodb";
-
 export default interface User {
-    _id?: Condition<ObjectId>,
     accessToken: string,
     refreshToken: string,
     key: string,
