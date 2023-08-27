60f065a073acde7bca5372a422c7242b1b7dd0c2
Ruk Doe <info@doany.io>
2023-08-27 10:27:12 +0900

    fix font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,23 @@
-import type { Metadata } from 'next'
-import { BIZ_UDPGothic } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { BIZ_UDPGothic } from "next/font/google";
+import "./globals.css";
 export const metadata: Metadata = {
-  title: 'Tweel',
-  description: 'Tool for Twitter',
-}
-const UD = BIZ_UDPGothic({weight: ['400', '700'], preload: false, display: 'block'})
+  title: "Tweel",
+  description: "Tool for Twitter",
+};
+const UD = BIZ_UDPGothic({
+  weight: ["400", "700"],
+  subsets: ["cyrillic"],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="ja">
-      <body className={UD.className + ' dark:bg-gray-900'}>{children}</body>
+      <body className={UD.className + " dark:bg-gray-900"}>{children}</body>
     </html>
-  )
+  );
 }
