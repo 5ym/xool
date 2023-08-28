1dfa31436799e5da85a02b600389ee3bdb13560c
Ruk Doe <info@doany.io>
2023-08-28 11:29:37 +0900

    fix

--- utils/codeBlock.tsx
+++ utils/codeBlock.tsx
@@ -1,10 +1,12 @@
-import hljs from 'highlight.js'
+import hljs from "highlight.js";
 
-export default function CodeBlock(props: { code: string, language: string }) {
-    const highlightedCode: string = hljs.highlight(props.code, {language: props.language}).value
-    return (
-        <pre className='overflow-scroll p-1 mb-4 bg-gray-800 text-white'>
-            <code dangerouslySetInnerHTML={{ __html: highlightedCode }} />
-        </pre>
-    )
+export default function CodeBlock(props: { code: string; language: string }) {
+  const highlightedCode: string = hljs.highlight(props.code, {
+    language: props.language,
+  }).value;
+  return (
+    <pre className="overflow-scroll p-1 mb-4 bg-gray-800 text-white">
+      <code dangerouslySetInnerHTML={{ __html: highlightedCode }} />
+    </pre>
+  );
 }
