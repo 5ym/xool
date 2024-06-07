52da9e09e580f35e1feec19b82858f47e8c205fa
Ruk Doe <info@doany.io>
2024-06-07 23:20:56 +0900

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,18 +3,18 @@ import { BIZ_UDPGothic, BIZ_UDPMincho } from "next/font/google";
 import "./globals.css";
 export const metadata: Metadata = {
   title: "Tweel",
-  description: "Tool for Twitter",
+  description: "Tool for 𝕏",
 };
 const gothic = BIZ_UDPGothic({
   weight: ["400", "700"],
   subsets: ["cyrillic"],
-  variable: '--font-gothic',
+  variable: "--font-gothic",
 });
 const mincho = BIZ_UDPMincho({
   weight: ["400", "700"],
   subsets: ["cyrillic"],
-  variable: '--font-mincho',
-})
+  variable: "--font-mincho",
+});
 
 export default function RootLayout({
   children,
@@ -22,7 +22,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ja" className={`${gothic.variable} ${mincho.variable} dark:bg-gray-900`}>
+    <html
+      lang="ja"
+      className={`${gothic.variable} ${mincho.variable} dark:bg-gray-900`}
+    >
       <body>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         Tweel Webhook
       </h2>
       <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
-        Webhookでツイートをできるようにします
+        Webhookでポストをできるようにします
       </p>
       {message !== undefined ? (
         <div
@@ -38,24 +38,10 @@ export default function Home() {
       )}
       <a
         href="/api/oauth"
-        className="text-white bg-[#1da1f2] hover:bg-[#1da1f2]/90 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 mb-6"
+        className="text-white bg-black hover:bg-[#222]/90 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 mb-6 inline-block"
       >
-        <svg
-          className="w-4 h-4 mr-2 -ml-1"
-          aria-hidden="true"
-          focusable="false"
-          data-prefix="fab"
-          data-icon="twitter"
-          role="img"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 512 512"
-        >
-          <path
-            fill="currentColor"
-            d="M459.4 151.7c.325 4.548 .325 9.097 .325 13.65 0 138.7-105.6 298.6-298.6 298.6-59.45 0-114.7-17.22-161.1-47.11 8.447 .974 16.57 1.299 25.34 1.299 49.06 0 94.21-16.57 130.3-44.83-46.13-.975-84.79-31.19-98.11-72.77 6.498 .974 12.99 1.624 19.82 1.624 9.421 0 18.84-1.3 27.61-3.573-48.08-9.747-84.14-51.98-84.14-102.1v-1.299c13.97 7.797 30.21 12.67 47.43 13.32-28.26-18.84-46.78-51.01-46.78-87.39 0-19.49 5.197-37.36 14.29-52.95 51.65 63.67 129.3 105.3 216.4 109.8-1.624-7.797-2.599-15.92-2.599-24.04 0-57.83 46.78-104.9 104.9-104.9 30.21 0 57.5 12.67 76.67 33.14 23.72-4.548 46.46-13.32 66.6-25.34-7.798 24.37-24.37 44.83-46.13 57.83 21.12-2.273 41.58-8.122 60.43-16.24-14.29 20.79-32.16 39.31-52.63 54.25z"
-          ></path>
-        </svg>
-        Sign in with Twitter
+        <span>Sign in with</span>
+        <span className="text-2xl ml-2 leading-6 align-text-bottom">𝕏</span>
       </a>
       <h3 className="text-4xl font-extrabold dark:text-white">プライバシー</h3>
       <p className="mb-3 text-gray-500 dark:text-gray-400">
@@ -152,7 +138,7 @@ async function KeyInfo(props: { wkey: string }) {
               target="_blank"
               href="https://x.com"
             >
-              twitter
+              𝕏
             </a>
             側でアカウントを切り替えてから下記で再ログイン
           </h3>
