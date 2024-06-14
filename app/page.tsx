dea58e2115fd96a3d073dfe322b290afc417eeae
Ruk Doe <info@doany.io>
2024-06-14 01:12:59 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
         className="text-white bg-black hover:bg-[#222]/90 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center dark:focus:ring-[#1da1f2]/55 mb-6 inline-block"
       >
         <span>Sign in with</span>
-        <span className="text-2xl ml-2 leading-6 align-text-bottom">𝕏</span>
+        <span className="text-2xl ml-2 leading-6 align-bottom">𝕏</span>
       </a>
       <h3 className="text-4xl font-extrabold dark:text-white">プライバシー</h3>
       <p className="mb-3 text-gray-500 dark:text-gray-400">
