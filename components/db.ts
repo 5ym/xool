a0cb7c73fd90a5523ffac4d9ffacf15fa704dff4
Ruk Doe <info@doany.io>
2024-06-14 10:51:55 +0900

    fix db

--- components/db.ts
+++ components/db.ts
@@ -3,5 +3,5 @@ import { MongoClient } from "mongodb";
 export default async function mongo() {
   const client = new MongoClient("mongodb://root:toor@db:27017", {});
   const connect = await client.connect();
-  return connect.db(process.env.MONGO_INITDB_DATABASE);
+  return connect.db('xool');
 }
