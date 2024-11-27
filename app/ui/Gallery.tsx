3214d345d1c057fa7a7058cae99f0835111548f9
Ruk Doe <info@doany.io>
2024-11-28 03:20:57 +0900

    fix style and modal

--- app/ui/Gallery.tsx
+++ app/ui/Gallery.tsx
@@ -2,28 +2,57 @@
 
 import type { LImage } from "@/utils/Model";
 import type { WithId } from "mongodb";
+import { useRef, useState } from "react";
 import CopyButton from "./CopyButton";
 
 export default function Gallery({
 	imageList,
 }: { imageList: WithId<LImage>[] }) {
-	const onClickItem = () => {};
+	const [diaImage, setDiaImage] = useState("");
+	const dialog = useRef<HTMLDialogElement>(null);
+	const onClickItem = (fileName: string) => {
+		setDiaImage(fileName);
+		dialog.current?.showModal();
+	};
+	const closeDialog = () => {
+		dialog.current?.close();
+	};
 	return (
-		// biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
-		<div className="flex flex-wrap h-full gap-3" onClick={onClickItem}>
-			{imageList.map((image) => (
-				<div
-					key={image.fileName}
-					className="relative group/item grow max-h-[30%] cursor-pointer rounded-3xl overflow-hidden"
-				>
-					<CopyButton fileName={image.fileName} />
-					<img
-						src={`/images/${image.fileName}`}
-						alt="LGTM"
-						className="h-full w-full object-cover group-hover/item:w-auto mx-auto"
-					/>
+		<>
+			<div className="flex flex-wrap gap-3">
+				{imageList.map((image) => (
+					// biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+					<div
+						key={image.fileName}
+						className="relative group/item grow h-[250px] max-w-[500px] cursor-pointer rounded-3xl overflow-hidden bg-primary-content"
+						onClick={() => onClickItem(image.fileName)}
+					>
+						<CopyButton fileName={image.fileName} />
+						<img
+							src={`/images/${image.fileName}`}
+							alt="LGTM"
+							className="h-full w-full object-cover group-hover/item:object-contain mx-auto"
+						/>
+					</div>
+				))}
+			</div>
+			<dialog ref={dialog} id="my_modal_1" className="modal">
+				<div className="modal-box">
+					<img src={`/images/${diaImage}`} alt="LGTM" className="mx-auto" />
+					<div className="modal-action">
+						<form method="dialog">
+							<button type="button" className="btn" onClick={closeDialog}>
+								Close
+							</button>
+						</form>
+					</div>
 				</div>
-			))}
-		</div>
+				<form method="dialog" className="modal-backdrop">
+					<button type="button" onClick={closeDialog}>
+						close
+					</button>
+				</form>
+			</dialog>
+		</>
 	);
 }
